839d3d8541146f3a4961409d8f07e08f92b8406b
Julio Mena <hola@juliomena.mx>
2021-08-09 20:21:50 -0500

    landingPage

---
+++ asset/js/main.js
@@ -0,0 +1,57 @@
+//Card 01
+function changePic1() {
+    document.getElementById("myImg01").src = "img/kelvin_joseph_02.jpg";
+       }
+      function changePic2(){
+       document.getElementById("myImg01").src ="img/kelvin_joseph.jpeg";
+       }
+//Card 02
+function changePic3() {
+    document.getElementById("myImg02").src = "img/leighton-vander-esch_02.jpg";
+       }
+      function changePic4(){
+       document.getElementById("myImg02").src ="img/leighton-vander-esch.jpeg";
+       }
+
+//Card 03
+
+function changePic5() {
+    document.getElementById("myImg03").src = "img/dan_quinn_02.jpg";
+       }
+      function changePic6(){
+       document.getElementById("myImg03").src ="img/dan_quinn.jpg";
+       }
+
+//Card MB 01
+
+function changePic7() {
+    document.getElementById("myImg04").src = "img/osa_odighizuwa_02.jpg";
+       }
+      function changePic8(){
+       document.getElementById("myImg04").src ="img/osa_odighizuwa.jpg";
+       }
+
+//Card MB 02
+
+function changePic9() {
+    document.getElementById("myImg05").src = "img/antwaun_woods_02.jpeg";
+       }
+      function changePic10(){
+       document.getElementById("myImg05").src ="img/antwaun_woods.jpeg";
+       }
+
+//Card MB 03
+
+function changePic11() {
+    document.getElementById("myImg06").src = "img/jabril_cox_02.jpeg";
+       }
+      function changePic12(){
+       document.getElementById("myImg06").src ="img/jabril_cox.jpeg";
+       }
+
+//Dark Mode
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
